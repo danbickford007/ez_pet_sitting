51112a77496080e5d3ce7f5ce10af5b265ea810e
dan <danbickford007@yahoo.com>
2013-03-31 20:42:51 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EzPetSitting::Application.config.secret_token = '2614e822c3ce3bedc31fb7c86d46ab6b89f120723ec2f4b2c7974ccbd0bac5ac1084a5aea92c9086311524e070725857798c689445603ffd9624b8438f110eca'
